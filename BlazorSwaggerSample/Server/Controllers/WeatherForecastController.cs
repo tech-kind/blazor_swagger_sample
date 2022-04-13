956ea7b5fdce05af4a1fb5180ab0ec8740c1f7ed
TechKind <ken_hayakawa@tech-kind.biz>
2022-04-13 16:55:22 +0900

    プロジェクト ファイルを追加します。

---
+++ BlazorSwaggerSample/Server/Controllers/WeatherForecastController.cs
@@ -0,0 +1,35 @@
+using BlazorSwaggerSample.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorSwaggerSample.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get(int count)
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, count).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
